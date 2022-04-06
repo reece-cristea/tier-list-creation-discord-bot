ec47f89cfdaf5b30ad8c1e5cde56dd71c0a248df
reece.cristea <reece.cristea@mymail.champlain.edu>
2022-04-05 18:46:44 -0700

    added saving feature so the bot will load up all saved tier lists from file, added auto update on adding an entry to a list, still need to work on writeing tier list name to save file on creation

--- discordBot/main.py
+++ discordBot/main.py
@@ -10,6 +10,9 @@ client.main_list = []
 
 @client.event
 async def on_ready():
+    load_tier_lists()
+    print(len(client.main_list))
+    print(client.main_list[0].owner)
     for guild in client.guilds:
         if guild.name == GUILD:
             print(f"{client.user} is connected to the following guild: {guild.name}({guild.id})")
@@ -27,28 +30,36 @@ async def on_message(message):
     if message_contents[0] == "!createTierList":
         if len(message_contents) >= 2:
             title = message_contents[1]
-            entry = TierList(title, True, message.author)
+            entry = TierList(title, True, message.author.id)
             if len(message_contents) >= 3:
                 category = message_contents[2]
                 entry.change_category(category)
+            if len(message_contents) >= 4:
+                if message_contents[3] == "no":
+                    entry.change_save(False)
             client.main_list.append(entry)
         await message.channel.send("Created Tier List")
 
     if message_contents[0] == "!showTierList":
-        await message.channel.send("here")
         if len(message_contents) >= 2:
             tier_list = get_tier_list(message_contents[1], message.author)
-            entry_list = tier_list.get_entry_list()
+            entry_list = tier_list.list_of_entries
+            await message.channel.send(tier_list.title)
+            await message.channel.send(f"No.                            Name                                    Rank")
             count = 0
             for entry in entry_list:
                 count += 1
-                await message.channel.send(f"{count}. {entry.to_string()}")
+                await message.channel.send(f"{count}.                               {entry.to_string()}")
 
     if message_contents[0] == "!addEntryToTierList":
         await message.channel.send("here")
         if len(message_contents) == 4:
             tier_list = get_tier_list(message_contents[1], message.author)
-            tier_list.add_entry(message_contents[2], message_contents[3])
+            entry = TierListEntry(message_contents[2], message_contents[3])
+            print(tier_list)
+            tier_list.add_entry(entry.name, entry.rank)
+            if tier_list.save:
+                save_entry_to_list(entry, tier_list)
             await message.channel.send("created entry")
 
 
@@ -58,14 +69,46 @@ async def on_member_join(member):
         if guild.name == GUILD:
             for channel in guild.channels:
                 if channel.name == "general":
-                    await channel.send(f"{member.name} is a bing bong.")
+                    await channel.send(f"Hello {member.name}! Welcome to the server.")
 
 
 def get_tier_list(title, owner):
-    for tier_list in client.main_list:
-        if tier_list.get_title() == title and tier_list.get_owner() == owner:
-            return tier_list
+    print(title)
+    print(owner)
+    for x in range(len(client.main_list)):
+        print(client.main_list[x])
+        print(client.main_list[x].title)
+        print(client.main_list[x].owner)
+        print(client.main_list[x].title == title)
+        print(client.main_list[x].owner == owner.id)
+        if client.main_list[x].title == title and client.main_list[x].owner == owner.id:
+            return client.main_list[x]
     return None
+
+
+def load_tier_lists():
+    list_of_lists = open("save-tier-lists.txt")
+    num_of_lists = list_of_lists.readline()
+    for x in range(int(num_of_lists)):
+        tier_list_line = list_of_lists.readline()
+        properties = tier_list_line.split()
+        tier_list = TierList(properties[0], True, str(properties[1]))
+        for i in range(int(properties[2])):
+            entry_line = list_of_lists.readline().split()
+            entry = TierListEntry(entry_line[0], int(entry_line[1]))
+            tier_list.add_entry(entry.name, entry.rank)
+        client.main_list.append(tier_list)
+    list_of_lists.close()
+
+
+def save_entry_to_list(entry, tier_list):
+    save_file = open("save-tier-lists.txt")
+    num = int(save_file.readline())
+    for x in range(num):
+        title = save_file.readline().split()[0]
+        if tier_list.title == title:
+            entry_string = entry.to_string()
+            save_file.write(entry_string)
 
 
 class TierList:
@@ -98,6 +141,9 @@ class TierList:
     def change_category(self, category):
         self.category = category
 
+    def change_save(self, save):
+        self.save = save
+
 
 class TierListEntry:
     name: None
@@ -108,7 +154,7 @@ class TierListEntry:
         self.rank = rank
 
     def to_string(self):
-        entry = str(self.name + " " + self.rank)
+        entry = str(self.name + "                                    " + str(self.rank))
         return entry
 
 
